9626cf3916d1eee9df5d1b8b8a1fe5db7ba02696
Felipe S <felipe.s447@gmail.com>
2018-03-07 13:54:58 -0500

    Skeleton

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+
+            </div>
+        )
+    }
+}
+
+export default App

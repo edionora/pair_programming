ef39e3c403d49e3976575d319dd58854dc8a1efc
Erwin Dionora <edionora@ryerson.ca>
2018-03-09 11:16:20 -0500

    date and time

--- src/app.js
+++ src/app.js
@@ -30,8 +30,11 @@ export default class App extends React.Component {
     render() {
         this.getDarkSkyData('45.3226198', '-75.9219474');
         return (
+            
             <div className="app">
                 <h1>Test</h1>
+                <div className="text-right"><div id="date">{this.props.date.toLocaleDateString()}</div>
+                <div id="time">{this.props.date.toLocaleTimeString()}</div></div>
                 <Weather temp={this.state.temp}/>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './styles.css'  // my CSS sheet
 
 
-ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
-    document.getElementById('root'))
+// ReactDOM.render(
+//     <Router>
+//         <App/>
+//     </Router>,
+//     document.getElementById('root'))
+    function tick() {
+        ReactDOM.render(<App date={new Date()}/>, document.getElementById('root')
+        );
+    }
+    setInterval(tick, 1000);

c9bee0ec4dfac90407f9dab8f3f253528eb8082a
Erwin Dionora <edionora@ryerson.ca>
2018-03-09 12:03:16 -0500

    backend and weather

--- src/mirror_server/server.js
+++ src/mirror_server/server.js
@@ -4,6 +4,8 @@
 const express = require('express')
 // const bodyParser = require('body-parser')
 const cors = require('cors');
+const request = require('request');
+
 
 // App setup
 const app = express()
@@ -15,4 +17,24 @@ app.use(cors())
 app.get('/', (req, res) => res.send('hey'))
 
 // start server
-app.listen(8080, () => console.log('localhost:8080 is up!'))
+app.listen(8060, () => console.log('localhost:8080 is up!'))
+
+
+
+const getDarkSkyData = (lat, lon) => {
+    let apiKey = '22cc2cad4e250dafcf83e050ee45fe84';
+    let url = 'https://api.darksky.net/forecast/' + apiKey + '/' + lat + ',' + lon;
+    request(url, (err,res,body)=> {
+        const data = JSON.parse(body)
+        const temperature = data.currently.temperature
+        console.log(temperature);
+    })
+    // axios.get(url)
+    //     .then(res => { 
+    //         this.setState({
+    //             temp: 3
+    //         })
+    //     })
+}
+
+getDarkSkyData('45.3226198', '-75.9219474');
